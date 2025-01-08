f0469dbf7912caa7da681a0897b5599230fc0f3f
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-08 20:22:49 +0300

    chor: set configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: ['rickandmortyapi.com'],
+    },
 };
 
 export default nextConfig;
