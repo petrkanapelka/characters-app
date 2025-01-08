ca6dac7745f2343cf69deb47508ba5d56cdd57ba
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-08 20:51:29 +0300

    feat: move type Character to types.ts

--- assets/hooks/useCharacters.ts
+++ assets/hooks/useCharacters.ts
@@ -1,12 +1,6 @@
-import { Nullable } from './../types/types';
+import { Character, Nullable } from './../types/types';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-
-type Character = {
-    id: string;
-    name: string;
-    image: string;
-};
 
 export const useCharacters = (): Nullable<Character[]> => {
     const [characters, setCharacters] = useState<Nullable<Character[]>>(null);
--- assets/types/types.ts
+++ assets/types/types.ts
@@ -1,1 +1,7 @@
 export type Nullable<T> = null | T;
+
+export type Character = {
+    id: string;
+    name: string;
+    image: string;
+};
